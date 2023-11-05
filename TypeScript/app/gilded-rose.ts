4515d61b4c2c36bd904a23461142a8012704938b
Charles Rethman <rethman@me.com>
2023-11-06 10:24:53 +1300

    renamed methods to shift test to new, revised version

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,7 +17,7 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQualityRevised() {
+  updateQuality() {
     const updatedItems = this.items
       .filter(item => item && item.name)  // make sure there is an item and it has a name
       .map(item => {
@@ -51,7 +51,7 @@ export class GildedRose {
     return updatedItems;
   }
 
-  updateQuality() {
+  updateQualityOld() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
