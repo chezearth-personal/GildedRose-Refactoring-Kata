d417a745f9a2bcf7173ab8808b5419165a130b2e
Charles Rethman <rethman@me.com>
2023-11-06 16:46:28 +1300

    Added in a set of arrays of objects that follow each of the different types of rules

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,4 +1,5 @@
 import {kill} from "process";
+import {G} from "vitest/dist/types-0373403c";
 
 export class Item {
   name: string;
@@ -14,6 +15,13 @@ export class Item {
 
 export class GildedRose {
   items: Array<Item>;
+  /**
+   * Insert any objects with string names and define rates to have them follow the rules laid out in updateQuality 
+   */
+  type1 = [{ name: 'sulfuras', rate: 1 }];
+  type2 = [{ name: 'backstage passes', rate: 1 }];
+  type3 = [{ name: 'aged brie', rate: 1 }];
+  typedefault = [{ name: 'conjured', rate: 2 }];
 
   constructor(items = [] as Array<Item>) {
     this.items = items;
@@ -22,33 +30,35 @@ export class GildedRose {
   updateQuality() {
     this.items
       .forEach(item => {
-        if (!item || !item.name) { return; }                          // make sure there is an item and it has a name
-        if (item.name.substring(0, 8).toLowerCase() === 'sulfuras') { // The legendary items is set to max
+        const nm = item.name.toLowerCase();
+        if (!item || !item.name) { return; }                            // make sure there is an item and it has a name
+        if (this.type1.map(e => e.name).includes(nm.substring(0, 8))) { // The legendary items is set to max
           item.quality = 50;
           item.sellIn = 365;
           return item;
         }
-        if (item.name.substring(0, 16).toLowerCase() === 'backstage passes') {
+        if (this.type2.map(e => e.name).includes(nm.substring(0, 16))) {
           return item.sellIn > 10
-            ? this.degradeItem(item, 1)
+            ? this.degradeItem(item, this.type2.filter(e => e.name === nm)[0].rate)
             : item.sellIn > 5
-              ? this.degradeItem(item, 2)
+              ? this.degradeItem(item, this.type2.filter(e => e.name === nm)[0].rate  * 2)
               : item.sellIn > 0
-                ? this.degradeItem(item, 3)
-                : this.killItem(item)
+                ? this.degradeItem(item, this.type2.filter(e => e.name === nm)[0].rate * 3)
+                : this.expireItem(item)
         }
-        if (item.name.toLowerCase() === 'aged brie') {
-          return this.degradeItem(item, 1)
+        if (this.type3.map(e => e.name).includes(nm)) {
+          return this.degradeItem(item, this.type3.filter(e => e.name === nm)[0].rate);
         }
-        return this.defaultDegrade(item, -1)
+        const rate:number = this.typedefault.filter(e => e.name === nm)[0] ? this.typedefault.filter(e => e.name === nm)[0].rate : 1
+        return this.defaultDegrade(item, -1 * rate);
       });
     return this.items;
   }
 
-  defaultDegrade(item, rate) {
+  defaultDegrade(item: Item, rate: number) {
     return item.quality > 1
       ? item.sellIn > 0 ? this.degradeItem(item, rate) : this.degradeItem(item, rate * 2)
-      : this.killItem(item);
+      : this.expireItem(item);
   }
 
   degradeItem(item: Item, rate: number) {
@@ -58,7 +68,7 @@ export class GildedRose {
     return item;
   }
 
-  killItem(item: Item) {
+  expireItem(item: Item) {
     item.quality = 0;
     item.sellIn--;
     return item;
